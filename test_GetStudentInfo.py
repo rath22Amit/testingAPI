eca7ca2ce830f1d8a132cb7c0469304e0546c0a2
rath22Amit <amit1995rath22@gmail.com>
2020-08-31 10:43:24 +0530

    Student_Management

--- test_GetStudentInfo.py
+++ test_GetStudentInfo.py
@@ -1,6 +1,6 @@
 import json,jsonpath,requests
 
 def test_get_Student_info():
-    url='http://thetestingworldapi.com/api/studentsDetails/61017'
+    url='http://thetestingworldapi.com/api/studentsDetails/61019'
     response=requests.get(url)
     print(response.content)
